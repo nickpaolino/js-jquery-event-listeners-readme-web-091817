90393cdc6510076c2e877df3aec753368e4bff5d
Nick Paolino <github email address>
2017-11-03 09:39:53 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(ev){
+    if (ev.which === 71){
+      alert("G key pressed")
+    }
+    else {}
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 
 });
